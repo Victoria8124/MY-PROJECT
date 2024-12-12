01350ee26c31733d79eea30cde2810df733a7d32
Виктория <119865129+Victoria8124@users.noreply.github.com>
2024-12-12 03:06:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,22 +8,18 @@ import "./styles/main.scss";
                 zoom: 4
             });
 
-            // Метка 1 (Москва)
             var placemark1 = new ymaps.Placemark([55.7558, 37.6173], {
                 balloonContent: 'Это Москва'
             });
 
-            // Метка 2 (Санкт-Петербург)
             var placemark2 = new ymaps.Placemark([59.9343, 30.3351], {
                 balloonContent: 'Это Санкт-Петербург'
             });
 
-            // Метка 3 (Новосибирск)
             var placemark3 = new ymaps.Placemark([55.0084, 82.9357], {
                 balloonContent: 'Это Новосибирск'
             });
 
-            // Добавляем метки на карту
             map.geoObjects.add(placemark1);
             map.geoObjects.add(placemark2);
             map.geoObjects.add(placemark3);
